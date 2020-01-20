a9d8326369119e976f4f372ed4efa9e482ea8fcb
377823385@qq.com <huihui1107>
2020-01-20 19:23:04 +0800

    a

--- demo3logback/src/main/java/com/example/demo3log/logback/LogbackApplication.java
+++ demo3logback/src/main/java/com/example/demo3log/logback/LogbackApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LogbackApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(LogbackApplication.class, args);
     }
